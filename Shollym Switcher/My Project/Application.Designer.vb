230e8f616d2bf3a877c3dcacdbbcd95506bae7d2
Edin Jašarević <edinjasar14@gmail.com>
2017-08-09 18:46:02 +0200

    rename main form

--- Shollym Switcher/My Project/Application.Designer.vb
+++ Shollym Switcher/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Jasarsoft.Switcher.ShollymPatch.Form1
+            Me.MainForm = Global.Jasarsoft.Switcher.ShollymPatch.MainForm
         End Sub
     End Class
 End Namespace
