b7137afb2563b8af8bd084f1fbc9c951d1d022f4
Edin Jašarević <edinjasar14@gmail.com>
2017-08-09 20:28:47 +0200

    write comment

--- Shollym Switcher/Class/MessageTitle.vb
+++ Shollym Switcher/Class/MessageTitle.vb
@@ -1,30 +1,47 @@
 ﻿Public NotInheritable Class MessageTitle
 
+    'Title fields
     Private ReadOnly infoTitle As String
     Private ReadOnly warnTitle As String
     Private ReadOnly errorTitle As String
 
+    'Additional fileds
     Private separator As String = " | "
     Private application As String = "Shollym Switcher"
 
+    ''' <summary>
+    ''' Default class constructor
+    ''' </summary>
     Public Sub New()
         Me.infoTitle = Me.application + Me.separator + "Information"
         Me.warnTitle = Me.application + Me.separator + "Warning"
         Me.errorTitle = Me.application + Me.separator + "Error"
     End Sub
 
+    ''' <summary>
+    ''' Information caption text
+    ''' </summary>
+    ''' <returns>Caption information text</returns>
     Public ReadOnly Property InfoCaption As String
         Get
             Return Me.infoTitle
         End Get
     End Property
 
+    ''' <summary>
+    ''' Warning caption text
+    ''' </summary>
+    ''' <returns>Caption warning text</returns>
     Public ReadOnly Property WarnCaption As String
         Get
             Return Me.warnTitle
         End Get
     End Property
 
+    ''' <summary>
+    ''' Error caption text
+    ''' </summary>
+    ''' <returns>Caption error text</returns>
     Public ReadOnly Property ErrorCaption As String
         Get
             Return Me.errorTitle
