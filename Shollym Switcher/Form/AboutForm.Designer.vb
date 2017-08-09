4db39d0b03179b0fad810a751f50b5398257c56b
Edin Jašarević <edinjasar14@gmail.com>
2017-08-09 19:19:08 +0200

    create control in about form

--- Shollym Switcher/Form/AboutForm.Designer.vb
+++ Shollym Switcher/Form/AboutForm.Designer.vb
@@ -22,7 +22,149 @@ Partial Class AboutForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.labelHorizontalLine = New System.Windows.Forms.Label()
+        Me.labelDescription = New System.Windows.Forms.Label()
+        Me.textDescription = New System.Windows.Forms.TextBox()
+        Me.labelWebste = New System.Windows.Forms.Label()
+        Me.textWebsite = New System.Windows.Forms.TextBox()
+        Me.labelLicense = New System.Windows.Forms.Label()
+        Me.labelDeveloper = New System.Windows.Forms.Label()
+        Me.textLicense = New System.Windows.Forms.TextBox()
+        Me.textDeveloper = New System.Windows.Forms.TextBox()
+        Me.labelVersion = New System.Windows.Forms.Label()
+        Me.textVersion = New System.Windows.Forms.TextBox()
+        Me.textTitle = New System.Windows.Forms.TextBox()
+        Me.labelTitle = New System.Windows.Forms.Label()
+        Me.buttonClose = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'labelHorizontalLine
+        '
+        Me.labelHorizontalLine.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.labelHorizontalLine.Location = New System.Drawing.Point(8, 192)
+        Me.labelHorizontalLine.Name = "labelHorizontalLine"
+        Me.labelHorizontalLine.Size = New System.Drawing.Size(320, 2)
+        Me.labelHorizontalLine.TabIndex = 12
+        '
+        'labelDescription
+        '
+        Me.labelDescription.Location = New System.Drawing.Point(8, 158)
+        Me.labelDescription.Name = "labelDescription"
+        Me.labelDescription.Size = New System.Drawing.Size(80, 22)
+        Me.labelDescription.TabIndex = 5
+        Me.labelDescription.Text = "Description:"
+        Me.labelDescription.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'textDescription
+        '
+        Me.textDescription.Location = New System.Drawing.Point(94, 158)
+        Me.textDescription.MaxLength = 512
+        Me.textDescription.Name = "textDescription"
+        Me.textDescription.ReadOnly = True
+        Me.textDescription.Size = New System.Drawing.Size(234, 22)
+        Me.textDescription.TabIndex = 11
+        '
+        'labelWebste
+        '
+        Me.labelWebste.Location = New System.Drawing.Point(8, 130)
+        Me.labelWebste.Name = "labelWebste"
+        Me.labelWebste.Size = New System.Drawing.Size(80, 22)
+        Me.labelWebste.TabIndex = 4
+        Me.labelWebste.Text = "Website:"
+        Me.labelWebste.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'textWebsite
+        '
+        Me.textWebsite.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.textWebsite.Location = New System.Drawing.Point(94, 130)
+        Me.textWebsite.MaxLength = 512
+        Me.textWebsite.Name = "textWebsite"
+        Me.textWebsite.ReadOnly = True
+        Me.textWebsite.Size = New System.Drawing.Size(234, 22)
+        Me.textWebsite.TabIndex = 10
+        '
+        'labelLicense
+        '
+        Me.labelLicense.Location = New System.Drawing.Point(8, 102)
+        Me.labelLicense.Name = "labelLicense"
+        Me.labelLicense.Size = New System.Drawing.Size(80, 22)
+        Me.labelLicense.TabIndex = 3
+        Me.labelLicense.Text = "License:"
+        Me.labelLicense.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'labelDeveloper
+        '
+        Me.labelDeveloper.Location = New System.Drawing.Point(8, 74)
+        Me.labelDeveloper.Name = "labelDeveloper"
+        Me.labelDeveloper.Size = New System.Drawing.Size(80, 22)
+        Me.labelDeveloper.TabIndex = 2
+        Me.labelDeveloper.Text = "Developer:"
+        Me.labelDeveloper.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'textLicense
+        '
+        Me.textLicense.Location = New System.Drawing.Point(94, 102)
+        Me.textLicense.MaxLength = 512
+        Me.textLicense.Name = "textLicense"
+        Me.textLicense.ReadOnly = True
+        Me.textLicense.Size = New System.Drawing.Size(234, 22)
+        Me.textLicense.TabIndex = 9
+        '
+        'textDeveloper
+        '
+        Me.textDeveloper.Location = New System.Drawing.Point(94, 74)
+        Me.textDeveloper.MaxLength = 512
+        Me.textDeveloper.Name = "textDeveloper"
+        Me.textDeveloper.ReadOnly = True
+        Me.textDeveloper.Size = New System.Drawing.Size(234, 22)
+        Me.textDeveloper.TabIndex = 8
+        '
+        'labelVersion
+        '
+        Me.labelVersion.Location = New System.Drawing.Point(8, 46)
+        Me.labelVersion.Name = "labelVersion"
+        Me.labelVersion.Size = New System.Drawing.Size(80, 22)
+        Me.labelVersion.TabIndex = 1
+        Me.labelVersion.Text = "Version:"
+        Me.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'textVersion
+        '
+        Me.textVersion.Location = New System.Drawing.Point(94, 46)
+        Me.textVersion.MaxLength = 512
+        Me.textVersion.Name = "textVersion"
+        Me.textVersion.ReadOnly = True
+        Me.textVersion.Size = New System.Drawing.Size(234, 22)
+        Me.textVersion.TabIndex = 7
+        '
+        'textTitle
+        '
+        Me.textTitle.Location = New System.Drawing.Point(94, 18)
+        Me.textTitle.MaxLength = 512
+        Me.textTitle.Name = "textTitle"
+        Me.textTitle.ReadOnly = True
+        Me.textTitle.Size = New System.Drawing.Size(234, 22)
+        Me.textTitle.TabIndex = 6
+        '
+        'labelTitle
+        '
+        Me.labelTitle.Location = New System.Drawing.Point(8, 18)
+        Me.labelTitle.Name = "labelTitle"
+        Me.labelTitle.Size = New System.Drawing.Size(80, 22)
+        Me.labelTitle.TabIndex = 0
+        Me.labelTitle.Text = "Title:"
+        Me.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'buttonClose
+        '
+        Me.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.buttonClose.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.buttonClose.Location = New System.Drawing.Point(8, 197)
+        Me.buttonClose.Name = "buttonClose"
+        Me.buttonClose.Size = New System.Drawing.Size(320, 37)
+        Me.buttonClose.TabIndex = 13
+        Me.buttonClose.Text = "CLOSE"
+        Me.buttonClose.UseVisualStyleBackColor = True
         '
         'AboutForm
         '
@@ -30,6 +172,20 @@ Partial Class AboutForm
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(340, 246)
         Me.ControlBox = False
+        Me.Controls.Add(Me.labelHorizontalLine)
+        Me.Controls.Add(Me.labelDescription)
+        Me.Controls.Add(Me.textDescription)
+        Me.Controls.Add(Me.labelWebste)
+        Me.Controls.Add(Me.textWebsite)
+        Me.Controls.Add(Me.labelLicense)
+        Me.Controls.Add(Me.labelDeveloper)
+        Me.Controls.Add(Me.textLicense)
+        Me.Controls.Add(Me.textDeveloper)
+        Me.Controls.Add(Me.labelVersion)
+        Me.Controls.Add(Me.textVersion)
+        Me.Controls.Add(Me.textTitle)
+        Me.Controls.Add(Me.labelTitle)
+        Me.Controls.Add(Me.buttonClose)
         Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
@@ -41,6 +197,22 @@ Partial Class AboutForm
         Me.Text = "Shollym Switcher | About"
         Me.TopMost = True
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents labelHorizontalLine As Label
+    Friend WithEvents labelDescription As Label
+    Friend WithEvents textDescription As TextBox
+    Friend WithEvents labelWebste As Label
+    Friend WithEvents textWebsite As TextBox
+    Friend WithEvents labelLicense As Label
+    Friend WithEvents labelDeveloper As Label
+    Friend WithEvents textLicense As TextBox
+    Friend WithEvents textDeveloper As TextBox
+    Friend WithEvents labelVersion As Label
+    Friend WithEvents textVersion As TextBox
+    Friend WithEvents textTitle As TextBox
+    Friend WithEvents labelTitle As Label
+    Friend WithEvents buttonClose As Button
 End Class
